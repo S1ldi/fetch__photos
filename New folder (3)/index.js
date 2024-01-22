68ac240417254d1f37a9a69138fec577d65508fa
S1ldi <148056959+S1ldi@users.noreply.github.com>
2024-01-22 16:05:06 +0100

    Add files via upload

---
+++ New folder (3)/index.js
@@ -0,0 +1,33 @@
+function loadImages() {
+    let img1 = document.getElementById("img1");
+    let img2 = document.getElementById("img2");
+    let img3 = document.getElementById("img3");
+    let img4 = document.getElementById("img4");
+  
+    fetch("https://picsum.photos/367").then(
+      (response) => (img1.src = response.url)
+    );
+  
+    fetch("https://picsum.photos/367").then(
+      (response) => (img2.src = response.url)
+    );
+  
+    fetch("https://picsum.photos/367").then(
+      (response) => (img3.src = response.url)
+    );
+  
+    fetch("https://picsum.photos/367").then(
+      (response) => (img4.src = response.url)
+    );
+  }
+  
+  function toggleGrayscale() {
+    const images = document.querySelectorAll("img");
+    images.forEach((img) => {
+      img.classList.toggle("grayscale");
+    });
+  }
+  
+  toggleSwitch.addEventListener("change", function () {
+    toggleGrayscale();
+  });
